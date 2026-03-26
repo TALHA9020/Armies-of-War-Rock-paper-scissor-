2189f1fb1c1ecddf63578ca2f5d4e6eac1bdb91f
TALHA9020 <talhasajidabbasi90209050@gmail.com>
2026-03-26 17:13:03 +0500

    Update DataModels.kt

--- app/src/main/java/com/armies/ofwar/DataModels.kt
+++ app/src/main/java/com/armies/ofwar/DataModels.kt
@@ -3,21 +3,45 @@ package com.armies.ofwar
 import androidx.compose.ui.graphics.Color
 import java.io.Serializable
 
+// یونٹس کی مختلف اقسام
 enum class UnitType {
     ROCK, PAPER, SCISSORS, NONE
 }
 
+// گیم کے مختلف مراحل
 enum class TurnPhase {
-    ATTACK, MOVE
+    SELECT_PLAYER_COLOR, // نیا: رنگ منتخب کرنے کا مرحلہ
+    SETUP_ENEMIES,       // نیا: دشمنوں کی تعداد منتخب کرنا
+    ATTACK, 
+    MOVE
 }
 
+// ہر چوکی میں موجود یونٹس کی تفصیل
+data class UnitCounts(
+    var rocks: Int = 0,
+    var papers: Int = 0,
+    var scissors: Int = 0
+) : Serializable {
+    val total: Int get() = rocks + papers + scissors
+}
+
+// چوکی (Outpost) کا ماڈل
+data class Outpost(
+    val id: Int,
+    val ownerId: Int,
+    var units: UnitCounts,
+    val posX: Float, // نقشے پر پوزیشن
+    val posY: Float
+) : Serializable
+
+// فوج (Army) کا اپڈیٹڈ ماڈل
 data class Army(
     val id: Int,
     val name: String,
     val color: Color,
     val isUserControlled: Boolean,
-    var armyCount: Int = 20,
-    var allianceId: Int
+    var allianceId: Int,
+    val outposts: MutableList<Outpost> = mutableListOf()
 ) : Serializable
 
 object RPSRules {
