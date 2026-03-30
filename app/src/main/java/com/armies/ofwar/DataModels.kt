b2fe5b1cbce1ff1c165cbe40a6ea15ec7cafbf6d
TALHA9020 <talhasajidabbasi90209050@gmail.com>
2026-03-30 20:07:03 +0500

    Replace DataModels.kt with updated enums and data classes implementing Serializable

--- app/src/main/java/com/armies/ofwar/DataModels.kt
+++ app/src/main/java/com/armies/ofwar/DataModels.kt
@@ -1,38 +1,35 @@
 package com.armies.ofwar
 
-import androidx.compose.ui.graphics.Color
 import java.io.Serializable
 
-enum class UnitType { ROCK, PAPER, SCISSORS, NONE }
-
-enum class TerritoryLevel(val label: String) { 
-    POST("پوسٹ"), CITY("شہر"), PROVINCE("صوبہ"), COUNTRY("ملک"), 
-    CONTINENT("بر اعظم"), PLANET("سیارہ"), SOLAR_SYSTEM("نظام شمسی"), 
-    GALAXY("کہکشاں"), UNIVERSE("کائنات") 
+enum class Continent : Serializable {
+    AFRICA,
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    AUSTRALIA,
+    ANTARCTICA
 }
 
-data class UnitCounts(
-    var rocks: Int = 0,
-    var papers: Int = 0,
-    var scissors: Int = 0
-) : Serializable {
-    val total: Int get() = rocks + papers + scissors
+enum class RPSChoice : Serializable {
+    ROCK,
+    PAPER,
+    SCISSORS
 }
 
-data class Outpost(
-    val id: Int,
-    var ownerId: Int,
-    var units: UnitCounts,
-    var posX: Float,
-    var posY: Float,
-    var level: TerritoryLevel = TerritoryLevel.POST
-) : Serializable
+enum class GamePhase : Serializable {
+    READY,
+    PLAYING,
+    FINISHED
+}
 
-data class Army(
-    val id: Int,
-    val name: String,
-    val color: Color,
-    val isUserControlled: Boolean,
-    var cards: MutableList<String> = mutableListOf(),
-    val outposts: MutableList<Outpost> = mutableListOf()
-) : Serializable
+data class Territory(val name: String, val continent: Continent) : Serializable
+
+data class Player(val name: String, val territory: Territory) : Serializable
+
+data class GameState(val players: List<Player>, val currentPhase: GamePhase) : Serializable
+
+data class RPSRound(val player1Choice: RPSChoice, val player2Choice: RPSChoice) : Serializable
+
+data class BattleResult(val winner: Player, val round: RPSRound) : Serializable
